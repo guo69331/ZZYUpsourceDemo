a9a402d0a36d6062d2d2a0f42d3e4e5c79724e53
zhangzeyu <zeyu930213@gmail.com>
2018-05-23 15:22:59 +0800

    test

--- UpSourceDemo/UpSourceDemo/ViewController.swift
+++ UpSourceDemo/UpSourceDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // 修改注释
+        title = "test"
     }
 
     override func didReceiveMemoryWarning() {
